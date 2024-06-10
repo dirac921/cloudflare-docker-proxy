9e791ea55b73493133461c9391e270f893c3d05b
dirac <jian_xv@foxmail.com>
2024-06-10 09:32:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.dirac.eu.org": "https://registry-1.docker.io",
+  "quay.dirac.eu.org": "https://quay.io",
+  "gcr.dirac.eu.org": "https://gcr.io",
+  "k8s-gcr.dirac.eu.org": "https://k8s.gcr.io",
+  "k8s.dirac.eu.org": "https://registry.k8s.io",
+  "ghcr.dirac.eu.org": "https://ghcr.io",
+  "cloudsmith.dirac.eu.org": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
